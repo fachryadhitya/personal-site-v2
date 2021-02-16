e4f5874b5a49a189fd426fe6b972e08f21892f08
FachryAdhitya <fachryadhitya@gmail.com>
2021-02-16 22:17:41 +0700

    change head title

--- pages/About/index.js
+++ pages/About/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { MainAbout } from "../../components/About/MainAbout";
 import { Footer } from "../../components/Home/Footer";
 import { Navbar } from "../../components/Navbar/Navbar";
@@ -5,6 +6,9 @@ import { Navbar } from "../../components/Navbar/Navbar";
 const About = () => {
   return (
     <>
+      <Head>
+        <title>About</title>
+      </Head>
       <Navbar />
       <MainAbout />
       <Footer />
